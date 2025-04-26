76cf5ffc2916b310068af7efc5adb0d5fd52645b
Brenda Silva Machado <87547436+Brenda-Machado@users.noreply.github.com>
2025-04-26 17:40:09 -0300

    Update cnn.py

--- src/cnn.py
+++ src/cnn.py
@@ -2,15 +2,12 @@
 Brenda Silva Machado - 21101954
 2024/2
 
-Version with Beta Distribution
-
 """
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.distributions import Beta
 import pickle
 
 class CNN(nn.Module):
@@ -24,14 +21,17 @@ class CNN(nn.Module):
         self.fc1 = nn.Linear(self._conv_output_size, 512)
         self.fc2 = nn.Linear(512, 512)
         
-        self.alpha_steering = nn.Linear(512, 1)
-        self.beta_steering = nn.Linear(512, 1)
-        self.alpha_brake = nn.Linear(512, 1)
-        self.beta_brake = nn.Linear(512, 1)
-        self.alpha_throttle = nn.Linear(512, 1)
-        self.beta_throttle = nn.Linear(512, 1)
+        self.steering = nn.Linear(512, 1)  
+        self.brake_throttle = nn.Linear(512, 2) 
+        self.mse_loss = nn.MSELoss()
+        self.loss = []
 
-        self.loss_for_statistics = []
+        self._initialize_weights()
+        
+    def _initialize_weights(self):
+        for layer in [self.conv1, self.conv2, self.conv3, self.fc1, self.fc2, self.steering, self.brake_throttle]:
+            if isinstance(layer, nn.Linear) or isinstance(layer, nn.Conv2d):
+                nn.init.kaiming_normal_(layer.weight, mode='fan_out', nonlinearity='relu')
         
     def conv_output(self, shape):
         input = torch.zeros(1, *shape)
@@ -46,30 +46,19 @@ class CNN(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         
-        alpha_steering_output = F.softplus(self.alpha_steering(x)) + 1e-3
-        beta_steering_output = F.softplus(self.beta_steering(x)) + 1e-3
-        alpha_brake_output = F.softplus(self.alpha_brake(x)) + 1e-3
-        beta_brake_output = F.softplus(self.beta_brake(x)) + 1e-3
-        alpha_throttle_output = F.softplus(self.alpha_throttle(x)) + 1e-3
-        beta_throttle_output = F.softplus(self.beta_throttle(x)) + 1e-3
+        steering_output = self.steering(x) 
+        brake_throttle_output = self.brake_throttle(x) 
         
-        return alpha_steering_output, beta_steering_output, alpha_brake_output, beta_brake_output, alpha_throttle_output, beta_throttle_output
+        return steering_output, brake_throttle_output
 
-    def compute_loss(self, steering_pred_alpha, steering_pred_beta, throttle_pred_alpha, throttle_pred_beta, brake_pred_alpha, brake_pred_beta, steering_real, throttle_real, brake_real):
-        steering_loss = self.compute_log_prob(steering_pred_alpha, steering_pred_beta, steering_real)
-        throttle_loss = self.compute_log_prob(throttle_pred_alpha, throttle_pred_beta, throttle_real)
-        brake_loss = self.compute_log_prob(brake_pred_alpha, brake_pred_beta, brake_real)
+    def compute_loss(self, steering_pred, throttle_brake_pred, steering_real, throttle_real, brake_real):
+        steering_loss = self.mse_loss(steering_pred.squeeze(1), steering_real)
+        throttle_loss = self.mse_loss(throttle_brake_pred[:, 0], throttle_real)
+        brake_loss = self.mse_loss(throttle_brake_pred[:, 1], brake_real)
 
         total_loss = steering_loss + throttle_loss + brake_loss
         
-        return -total_loss.mean()
-
-    def compute_log_prob(self, alpha, beta, expert_action):
-        beta_dist = Beta(alpha, beta)
-        expert_action = expert_action.clamp(1e-6, 1 - 1e-6)
-        log_prob = beta_dist.log_prob(expert_action)
-
-        return log_prob
+        return total_loss
 
     def train_model(self, dataloader, epochs, learning_rate):
         optimizer = optim.Adam(self.parameters(), lr=learning_rate)
@@ -82,20 +71,19 @@ class CNN(nn.Module):
             for i, data in enumerate(dataloader, 0):
                 states, actions_real = data
                 
-                steering_real = (actions_real[:, 0]+ 1)/2
+                steering_real = actions_real[:, 0]
                 throttle_real = actions_real[:, 1]
                 brake_real = actions_real[:, 2]
-
+                
                 optimizer.zero_grad()
 
-                steering_pred_alpha, steering_pred_beta, throttle_pred_alpha, throttle_pred_beta, brake_pred_alpha, brake_pred_beta = self(states)
-
-                loss = self.compute_loss(steering_pred_alpha, steering_pred_beta, throttle_pred_alpha, throttle_pred_beta, brake_pred_alpha, brake_pred_beta, steering_real, throttle_real, brake_real)
+                steering_pred, throttle_brake_pred = self(states)
+                loss = self.compute_loss(steering_pred, throttle_brake_pred, steering_real, throttle_real, brake_real)
                 loss.backward()
                 optimizer.step()
                 
                 running_loss += loss.item()
-                self.loss_for_statistics.append((i, loss.item()))
+                # self.loss.append((i, loss.item()))
 
             print(f'[Mean loss: {running_loss/len(dataloader)}]')
 
@@ -103,7 +91,7 @@ class CNN(nn.Module):
     def save_model(self, path):
         torch.save(self.state_dict(), path)
         with open('loss_best.pkl','wb') as f:
-            pickle.dump(self.loss_for_statistics, f)
+            pickle.dump(self.loss, f)
 
 
 
